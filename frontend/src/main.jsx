c6756e6a5175fb64c980638acfeda512852f2da1
Hardik Garg <hardikg2907@gmail.com>
2023-10-04 23:40:31 +0530

    backend url modified

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { AuthContextProvider } from "./context/AuthContext.jsx";
 import { MusicProvider } from "./context/MusicContext.jsx";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://aud-kryn183hm-hardikg2907.vercel.app/api/";
+axios.defaults.baseURL = "https://aud-io-two.vercel.app/api/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
