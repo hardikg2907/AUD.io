43253ecf9d24f99e1e3d8a533df7a80b7da3d2d3
iamparth <parthgenius.gps@gmail.com>
2023-10-05 13:08:27 +0530

    made refinements

--- frontend/src/components/AudioTile.jsx
+++ frontend/src/components/AudioTile.jsx
@@ -41,6 +41,7 @@ const AudioTile = ({ submission, page, setRender, allSongs, setIsLoading }) => {
   const { handleSetSong, activeSong, isPlaying, setIsPlaying } =
     useMusicContext();
   const navigate = useNavigate();
+  console.log(submission);
 
   const deleteMusic = async () => {
     setIsLoading(true);
@@ -69,11 +70,10 @@ const AudioTile = ({ submission, page, setRender, allSongs, setIsLoading }) => {
   return (
     <div
       key={submission._id}
-      className={`mb-6 hover:shadow-lg transition transform hover:-translate-y-2 cursor-pointer rounded-lg overflow-hidden w-32 h-44 flex justify-center items-center bg-[#3f3f3f] group duration-300 ease-in-out opacity-80 hover:opacity-100 px-2 ${
-        activeSong?._id === submission?._id
-          ? "-translate-y-2 shadow-lg opacity-100"
-          : ""
-      }`}
+      className={`mb-6 hover:shadow-lg transition transform hover:-translate-y-2 cursor-pointer rounded-lg overflow-hidden w-32 h-44 flex justify-center items-center bg-[#3f3f3f] group duration-300 ease-in-out opacity-80 hover:opacity-100 px-2 ${activeSong?._id === submission?._id
+        ? "-translate-y-2 shadow-lg opacity-100"
+        : ""
+        }`}
       onClick={() => navigate(`/my-submissions/${submission._id}`)}
     >
       <div className="absolute w-full flex justify-between top-2 mr-2">
@@ -107,8 +107,9 @@ const AudioTile = ({ submission, page, setRender, allSongs, setIsLoading }) => {
               toggleLike();
             }}
           >
-            {!liked && <AiOutlineHeart title="Add to Favourites" />}
-            {liked && <FcLike title="Remove from Favourites" />}
+            {!liked ?
+              <AiOutlineHeart title="Add to Favourites" /> :
+              <FcLike title="Remove from Favourites" />}
           </button>
         )}
       </div>
@@ -125,15 +126,14 @@ const AudioTile = ({ submission, page, setRender, allSongs, setIsLoading }) => {
       <div className="bg-[#383838] absolute self-end z-10 w-full px-2 py-1 h-1/3 shadow">
         <div className="flex justify-between items-center relative">
           <div
-            className={`absolute right-1 rounded-full h-5 w-5 group-hover:opacity-100 transition-all duration-300 ease-in-out opacity-0 scale-150 -mt-8 text-lg text-gray-900 bg-gray-200 flex justify-center items-center hover:scale-[1.7] ${
-              activeSong?._id === submission?._id ? "opacity-100" : ""
-            }`}
+            className={`absolute right-1 rounded-full h-5 w-5 group-hover:opacity-100 transition-all duration-300 ease-in-out opacity-0 scale-150 -mt-8 text-lg text-gray-900 bg-gray-200 flex justify-center items-center hover:scale-[1.7] ${activeSong?._id === submission?._id ? "opacity-100" : ""
+              }`}
             onClick={
               activeSong?._id === submission?._id && isPlaying
                 ? (e) => {
-                    setIsPlaying(false);
-                    e.stopPropagation();
-                  }
+                  setIsPlaying(false);
+                  e.stopPropagation();
+                }
                 : (event) => handleSetSong(submission, allSongs, event)
             }
           >
